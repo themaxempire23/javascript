579f192d750aaba6d507863467d72cf8e8a584da
MAX <83220484+themaxempire23@users.noreply.github.com>
2024-08-16 12:27:50 +0200

    Update app.js

--- db_test/app.js
+++ db_test/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 const db = mysql.createConnection({
     host: 'localhost',
