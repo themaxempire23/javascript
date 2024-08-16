f150da8feb21f3bd8ba399a08549b7fdd115ad35
HACKER99\Max <maxnda@gmail.com>
2024-08-16 12:22:40 +0200

    created app

---
+++ db_test/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Hacker99',
+    database: 'studentdb'
+})
